f5214c79a513facb0428aae06f4fc39b037c5beb
Арсений <199220097+Blik-5@users.noreply.github.com>
2026-04-14 15:41:38 +0300

    first commit

---
+++ PK5/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using PK5.FolderforModel; // добавляем созд папку из проекта для более удобного использования
+
+
+namespace PK5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static ModelDB DB = new ModelDB();
+
+        List<Table_Motorbike> Motorbikes = DB.Motorbike.ToList(); // Из бд таблица берётся
+        int AccNumber = 0; // на каком эл сейчас находимся
+
+        private void Loading() // пераоначальная (обычная) загрузка эл UserControlName
+        {
+            userControlName1.Fill(Motorbikes[AccNumber]); // заполнение 1 и 2-о User Control
+            userControlName2.Fill(Motorbikes[AccNumber + 1]);
+        }
+
+        private void Loading(bool Incr)
+        {
+            if (Incr == true && Motorbikes.Count > AccNumber + 2)
+                AccNumber++;
+            else if ((Incr == false && 0 <= AccNumber - 1))
+                AccNumber--;
+            else
+                return;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonRight_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonLeft_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
